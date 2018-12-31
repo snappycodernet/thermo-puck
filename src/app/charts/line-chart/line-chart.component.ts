7adc6e3911186feca8483cb063b925d070d2f098
Thaddeus Donaghue <thadd@Thaddeuss-MacBook-Pro-2.local>
2018-12-30 17:09:10 -0800

    add line chart implementation

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
+import { LINE_CHART_COLORS } from './../../shared/chart.colors';
 import { Component, OnInit } from '@angular/core';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+  {data: [46, 22, 59, 36, 49, 46], label: 'Image Recognition'},
+  {data: [61, 35, 27, 66, 55, 39], label: 'Forecasting'},
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +17,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend = true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
