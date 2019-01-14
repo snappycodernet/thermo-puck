9fc332fa9cb9befd813901ee5ccb69e8fe4f4f98
Thad Donaghue <thaddonaghue@yahoo.com>
2019-01-14 10:32:48 -0800

    implement range sensor chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,25 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Chart } from "chart.js";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+  selector: "app-pie-chart",
+  templateUrl: "./pie-chart.component.html",
+  styleUrls: ["./pie-chart.component.css"],
 })
 export class PieChartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  pieChartData: number[] =[350, 450, 120];
-  pieChartLabels: string[] = ['XYZ', 'Glasstic', 'Acme Hosting'];
-  colors: any[] = [
-    {
-      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
-      borderColor: '#eee'
-    }
-  ];
-  pieChartType = 'doughnut';
+  @Input() pieChartData: any[];
+  public interval: any;
+  public intervalDuration: number = 5000;
+  public range: number = 0;
+  public startCounter: number = 0;
+  public chart: Chart = null;
+  public pieChartLabels: string[] = ["Current"];
 
   ngOnInit() {
+    this.setData();
   }
 
+  setData() {
+    this.interval = setInterval(() => {
+      this.startGraph();
+    }, this.intervalDuration);
+  }
+
+  startGraph() {
+    this.range = Math.abs(
+      this.pieChartData[this.startCounter].RangeSensorReading
+    );
+    this.startCounter++;
+    this.updateChart();
+  }
+
+  updateChart() {
+    const canvas: any = document.querySelector("#pie-chart");
+    const ctx = canvas.getContext("2d");
+
+    if (!this.chart) {
+      this.chart = new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: this.pieChartLabels,
+          datasets: [
+            {
+              backgroundColor: ["limegreen", "#eee"],
+              data: [this.range, 100 - this.range],
+            },
+          ],
+        },
+        options: {
+          cutoutPercentage: 75,
+          legend: {
+            display: false,
+          },
+        },
+      });
+
+      this.chart.update();
+    } else {
+      if (this.range <= 25) {
+        this.chart.data.datasets[0].backgroundColor = ["red", "#eee"];
+      } else if (this.range <= 50) {
+        this.chart.data.datasets[0].backgroundColor = ["gold", "#eee"];
+      } else if (this.range <= 75) {
+        this.chart.data.datasets[0].backgroundColor = ["#a6f224", "#eee"];
+      } else {
+        this.chart.data.datasets[0].backgroundColor = ["limegreen", "#eee"];
+      }
+      this.chart.data.datasets[0].data = [this.range, 100 - this.range];
+
+      this.chart.update();
+    }
+  }
 }
